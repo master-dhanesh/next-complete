ae758ebececd0bb4de07e2c0fc1f7e86d1d69843
master-dhanesh <dhanesh1296@gmail.com>
2023-04-14 01:42:17 +0530

    Part 7

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,45 +1,47 @@
 import React, { useEffect, useState } from "react";
 const Gallery = () => {
     const [images, setImages] = useState([]);
-    const [username, setUsername] = useState("John");
-    const [password, setPassword] = useState("123456");
-
+    const [pagecount, setpagecount] = useState(1);
     const GetImages = async () => {
         const strimages = await fetch(
-            `https://picsum.photos/v2/list?page=2&limit=10`
+            `https://picsum.photos/v2/list?page=${pagecount}&limit=10`
         );
         const json = await strimages.json();
         setImages(json);
     };
 
     useEffect(() => {
-        console.log("Gallery Component Created");
-        return () => {
-            console.log("Gallery Component Destroyed");
-        };
-    }, [password]);
+        GetImages();
+    }, [pagecount]);
 
     return (
-        <ul className="list-group">
-            <button onClick={GetImages}>Get Images</button>
-            {images.length > 0
-                ? images.map((i) => (
-                      <li key={i.id} className="list-group-item">
-                          <img src={i.download_url} height={100} />
-                          <span className="ms-3">{i.author}</span>
-                      </li>
-                  ))
-                : "Loading..."}
-            <hr />
-            <h2>Username {username}</h2>
-            <button onClick={() => setUsername("Joh Wik")}>
-                Change Username
-            </button>
-            <h2>Password {password}</h2>
-            <button onClick={() => setPassword("Hellowww")}>
-                Change Password
-            </button>
-        </ul>
+        <>
+            <ul className="list-group">
+                {images.length > 0
+                    ? images.map((i) => (
+                          <li key={i.id} className="list-group-item">
+                              <img src={i.download_url} height={100} />
+                              <span className="ms-3">{i.author}</span>
+                          </li>
+                      ))
+                    : "Loading..."}
+            </ul>
+            <div className="mt-5 text-center">
+                <button
+                    onClick={() => setpagecount(pagecount - 1)}
+                    className="btn btn-dark"
+                >
+                    Prev
+                </button>
+                &nbsp;
+                <button
+                    onClick={() => setpagecount(pagecount + 1)}
+                    className="btn btn-dark"
+                >
+                    Next
+                </button>
+            </div>
+        </>
     );
 };
 
